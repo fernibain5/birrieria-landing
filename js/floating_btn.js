66a56effb314e999deda0aba99b2d5cb478b03c0
fernandobinowu <138172028+fernandobinowu@users.noreply.github.com>
2026-01-28 22:26:48 -0700

    Initial commit landing

---
+++ js/floating_btn.js
@@ -0,0 +1,3 @@
+document.getElementById("floatingBtn").addEventListener("click", function () {
+  window.location.href = "tel:+526629348727"; // Replace +1234567890 with your predetermined phone number
+});
